d1d08f4d5ee224baa3a060fa9e9f2a56cbd5cdda
JanGawronski <79528088+JanGawronski@users.noreply.github.com>
2021-02-23 16:30:32 +0100

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+void quicksort(int *tablica, int lewy, int prawy)
+{
+    int v=tablica[(lewy+prawy)/2];
+    int i,j,x;
+    i=lewy;
+    j=prawy;
+    do
+    {
+        while(tablica[i]<v) i++;
+        while(tablica[j]>v) j--;
+        if(i<=j)
+        {
+            x=tablica[i];
+            tablica[i]=tablica[j];
+            tablica[j]=x;
+            i++;
+            j--;
+        }
+    }
+    while(i<=j);
+    if(j>lewy) quicksort(tablica,lewy, j);
+    if(i<prawy) quicksort(tablica, i, prawy);
+}
+int main()
+{
+    return 0;
+}
